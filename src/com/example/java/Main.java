f04635e2cc8509c0dbdb5976869c2f3356d3d4de
Sravya Kada <sravya.kada@loment.net>
2020-04-26 14:40:56 -0500

    Some changes

--- src/com/example/java/Main.java
+++ src/com/example/java/Main.java
@@ -3,10 +3,10 @@ package com.example.java;
 public class Main {
 
     public static void main(String[] args) {
-         char c1 = '1';
+         char c = '1';
          char c2 = '2';
          char c3 = '3';
-        System.out.println("Char 1: " + c1);
+        System.out.println("Char 1: " + c);
         System.out.println("Char 2: " + c2);
         System.out.println("Char 3: " + c3);
 
